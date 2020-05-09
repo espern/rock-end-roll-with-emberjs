a00b6912f9dae9c2b602940ba18e3d09d8ca23f3
λoïc <loic.espern+github@gmail.com>
2020-05-09 14:55:03 +0200

    change style and generate new routes

--- app/router.js
+++ app/router.js
@@ -1,12 +1,17 @@
-import EmberRouter from '@ember/routing/router';
-import config from './config/environment';
+import EmberRouter from "@ember/routing/router";
+import config from "./config/environment";
 
 export default class Router extends EmberRouter {
   location = config.locationType;
   rootURL = config.rootURL;
 }
 
-Router.map(function() {
-  this.route('bands');
-  this.route('songs');
+Router.map(function () {
+  this.route("bands", function () {
+    this.route("songs");
+
+    this.route('band', {
+      path: ':slug'
+    });
+  });
 });
