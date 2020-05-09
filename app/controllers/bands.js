0f65966885a87936f5feea8eb42fb92281e35bc0
λoïc <loic.espern+github@gmail.com>
2020-05-09 22:11:43 +0200

    service injection

--- app/controllers/bands.js
+++ app/controllers/bands.js
@@ -2,12 +2,16 @@ import Controller from "@ember/controller";
 import { action } from "@ember/object";
 import Band from "rarwe/models/band";
 import { empty } from "@ember/object/computed";
+import { inject as service } from "@ember/service";
+import { dasherize } from "@ember/string";
 
 export default Controller.extend({
   isAddingBand: false,
   newBandName: "",
 
   isAddButtonDisabled: empty("newBandName"),
+
+  router: service(),
 
   addBand: action(function () {
     this.set("isAddingBand", true);
@@ -23,7 +27,11 @@ export default Controller.extend({
       name: this.newBandName,
     });
     this.model.pushObject(newBand);
-    this.set("newBandName", "");
-    this.cancelAddBand();
+    this.setProperties({
+      newBandName: "",
+      isAddingBand: false,
+    });
+    newBand.set("slug", dasherize(newBand.name));
+    this.router.transitionTo("bands.band.songs", newBand.slug);
   }),
 });
