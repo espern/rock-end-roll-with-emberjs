4e195bedcbadee9576b3d61c8413970b2b18ddbb
espern <loic.espern@gmail.com>
2020-05-20 16:25:05 +0200

    get the bands from API

--- app/routes/bands/band.js
+++ app/routes/bands/band.js
@@ -2,7 +2,6 @@ import Route from '@ember/routing/route'
 
 export default Route.extend({
   model(params) {
-    let bands = this.modelFor('bands')
-    return bands.find((band) => band.slug === params.slug)
+    return this.store.findRecord('band', params.id)
   }
 })
