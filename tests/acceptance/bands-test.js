a68f8d92877c451b11c4f2fa9442e9b6ac7a3dd4
espern <loic_espern@ultimatesoftware.com>
2020-05-25 12:32:50 +0200

    use of a custom helper to simplify tests

--- tests/acceptance/bands-test.js
+++ tests/acceptance/bands-test.js
@@ -1,5 +1,6 @@
 import { module, test } from 'qunit'
-import { visit, click, fillIn } from '@ember/test-helpers'
+import { visit } from '@ember/test-helpers'
+import { createBand } from 'rarwe/tests/helpers/custom-helpers'
 import { setupApplicationTest } from 'ember-qunit'
 import { setupMirage } from 'ember-cli-mirage/test-support'
 
@@ -16,10 +17,10 @@ module('Acceptance | Bands', function(hooks) {
       .dom('[data-test-rr=band-link]')
       .exists({ count: 2 }, 'All band links are rendered')
     assert
-      .dom('[data-test-rr=band-list-item]:firstchild')
+      .dom('[data-test-rr=band-list-item]:first-child')
       .hasText('Radiohead', 'The first band link contains the band name')
     assert
-      .dom('[data-test-rr=band-list-item]:lastchild')
+      .dom('[data-test-rr=band-list-item]:last-child')
       .hasText(
         'Long Distance Calling',
         'The other band link contains the band name'
@@ -29,9 +30,7 @@ module('Acceptance | Bands', function(hooks) {
   test('Create a band', async function(assert) {
     this.server.create('band', { name: 'Royal Blood' })
     await visit('/')
-    await click('[data-test-rr=new-band-label]')
-    await fillIn('[data-test-rr=new-band-input]', 'Caspian')
-    await click('[data-test-rr=new-band-button]')
+    await createBand('Caspian')
     assert
       .dom('[data-test-rr=band-list-item]')
       .exists({ count: 2 }, 'A new band link is rendered')
