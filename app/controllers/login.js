fef593442df952f39a97562ae4e020dd3dd05b18
espern <loic_espern@ultimatesoftware.com>
2020-06-02 12:17:41 +0200

    add tests and finish

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -4,9 +4,12 @@ import { action } from '@ember/object'
 
 export default Controller.extend({
   router: service(),
+  session: service(),
   signIn: action(async function(event) {
     event.preventDefault()
     let { email, password } = this
+    await this.session.authenticate('authenticator:credentials',
+      email, password);
     await this.router.transitionTo('bands')
   })
 })
--- mirage/config.js
+++ mirage/config.js
@@ -1,12 +1,24 @@
-export default function() {
+export default function () {
   this.get('/bands')
   this.get('/bands/:id')
   this.post('/bands')
-  this.get('/bands/:id/songs', function(schema, request) {
+  this.get('/bands/:id/songs', function (schema, request) {
     let { id } = request.params
     return schema.songs.where({ bandId: id })
   })
   this.post('/users')
+
+  this.post('/token', function (schema, request) {
+    let { username: email, password } =
+      JSON.parse(request.requestBody)
+    let users = schema.users.where({ email, password })
+    if (users.length === 1) {
+      return {
+        token: 'a.signed.jwt',
+        user_email: email
+      }
+    }
+  })
 
   // These comments are here to help you get started. Feel free to delete them.
 
